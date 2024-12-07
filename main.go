ebae135387e4105f5620d8526208a8d6617973c0
Miika Savela <miika.a.savela@gmail.com>
2024-12-07 13:19:08 +0200

    Add feedback when selecting invalid action

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 }
 
 func selectAction() {
+	fmt.Println("What do you want to do?")
+	fmt.Println("1 - Create teams for a practice")
+	fmt.Println("9 - Exit")
 	action, err := getAction()
 	if err != nil {
 		fmt.Println(err)
@@ -32,14 +35,14 @@ func selectAction() {
 		team2.PrintPlayers()
 	case "9":
 		return
+	default:
+		fmt.Printf("No action for %s. Select action from the list.\n\n", action)
 	}
 	selectAction()
 }
 
 func getAction() (action string, err error) {
-	fmt.Println("What do you want to do?")
-	fmt.Println("1 - Create teams for a practice")
-	fmt.Println("9 - Exit")
+
 	fmt.Scanln(&action)
 	if action == "" {
 		return "", errors.New("no input value provided")
