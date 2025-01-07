3a097930a9cdf33b694bd61e9bad089b99e08864
Miika Savela <miika.a.savela@gmail.com>
2025-01-07 20:03:46 +0200

    Add distributing players with goalies

--- team/distribute.go
+++ team/distribute.go
@@ -16,14 +16,39 @@ func Distribute(players []player.Player) (team1 Team, team2 Team, err error) {
 	}
 
 	sort.Sort(player.ByScore(players))
+	goalies, fieldPlayers := getGoalies(players)
+	distributeGoalies(goalies, &team1, &team2)
+	distributeFieldPlayers(fieldPlayers, &team1, &team2)
+	return team1, team2, nil
+}
 
-	for i, player := range players {
-		if (i+1)&2 == 0 {
-			team1.players = append(team1.players, player)
+func getGoalies(players []player.Player) (goalies, fieldPlayers []player.Player) {
+	for _, player := range players {
+		if player.IsGoalie {
+			goalies = append(goalies, player)
 		} else {
-			team2.players = append(team2.players, player)
+			fieldPlayers = append(fieldPlayers, player)
 		}
 	}
 
-	return team1, team2, nil
+	return goalies, fieldPlayers
+}
+
+func distributeGoalies(goalies []player.Player, team1, team2 *Team) {
+	for i, goalie := range goalies {
+		if (i+1)&2 == 0 {
+			team1.players = append(team1.players, goalie)
+		} else {
+			team2.players = append(team2.players, goalie)
+		}
+	}
+}
+func distributeFieldPlayers(fieldPlayers []player.Player, team1, team2 *Team) {
+	for i, fieldPlayer := range fieldPlayers {
+		if (i+1)&2 == 0 {
+			team1.players = append(team1.players, fieldPlayer)
+		} else {
+			team2.players = append(team2.players, fieldPlayer)
+		}
+	}
 }
