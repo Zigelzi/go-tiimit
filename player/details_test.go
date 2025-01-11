128083012de0c717d640bcf8fb0f595546a0a8ea
Miika Savela <miika.a.savela@gmail.com>
2025-01-11 14:46:00 +0200

    Move test cases outside the test function

--- player/details_test.go
+++ player/details_test.go
@@ -6,24 +6,25 @@ import (
 	"github.com/Zigelzi/go-tiimit/player"
 )
 
+var detailsTests = []struct {
+	name     string
+	player   player.Player
+	expected string
+}{
+	{
+		name:     "Player is goalie",
+		player:   player.Player{Name: "Matti Meikäläinen", IsGoalie: true},
+		expected: "Matti Meikäläinen [G]",
+	},
+	{
+		name:     "Player is not a goalie",
+		player:   player.Player{Name: "Matti Meikäläinen", IsGoalie: false},
+		expected: "Matti Meikäläinen",
+	},
+}
+
 func TestDetails(t *testing.T) {
-	tests := []struct {
-		name     string
-		player   player.Player
-		expected string
-	}{
-		{
-			name:     "Player is goalie",
-			player:   player.Player{Name: "Matti Meikäläinen", IsGoalie: true},
-			expected: "Matti Meikäläinen [G]",
-		},
-		{
-			name:     "Player is not a goalie",
-			player:   player.Player{Name: "Matti Meikäläinen", IsGoalie: false},
-			expected: "Matti Meikäläinen",
-		},
-	}
-	for _, testCase := range tests {
+	for _, testCase := range detailsTests {
 		t.Run(testCase.name, func(t *testing.T) {
 			actual := testCase.player.Details()
 			if actual != testCase.expected {
