65a480f9dfc40d085a690c310a016d0b25abb3b7
Miika Savela <miika.a.savela@gmail.com>
2025-02-11 21:00:42 +0200

    Add tests to get_preferences

--- player/get_preferences.go
+++ player/get_preferences.go
@@ -1,6 +1,8 @@
 package player
 
 func GetPreferences(players []Player) (goalies, fieldPlayers []Player) {
+	goalies = []Player{}
+	fieldPlayers = []Player{}
 	for _, player := range players {
 		if player.IsGoalie {
 			goalies = append(goalies, player)
