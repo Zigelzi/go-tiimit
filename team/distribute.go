f549ca0e74bde940e0244cb8c6099622817ef12f
Miika Savela <miika.a.savela@gmail.com>
2025-03-30 15:28:16 +0300

    Fix teams being distributed unevenly when number of goalies and field players is odd

--- team/distribute.go
+++ team/distribute.go
@@ -20,11 +20,30 @@ func Distribute(goalies, fieldPlayers []player.Player) (team1, team2 Team, err e
 }
 
 func distributePlayers(players []player.Player, team1, team2 *Team) {
+	startFromOdd := true
+	if len(team1.players) != len(team2.players) && !isEven(len(players)) {
+		// Need to switch order of distribution when number of players in teams isn't even and number of distributed players is odd.
+		// This is to ensure teams are distributed evenly.
+		startFromOdd = false
+	}
 	for i, distributedPlayer := range players {
-		if (i+1)%2 == 0 {
-			team1.players = append(team1.players, distributedPlayer)
+		if startFromOdd {
+			if isEven(i + 1) {
+				team1.players = append(team1.players, distributedPlayer)
+			} else {
+				team2.players = append(team2.players, distributedPlayer)
+			}
+
 		} else {
-			team2.players = append(team2.players, distributedPlayer)
+			if isEven(i) {
+				team1.players = append(team1.players, distributedPlayer)
+			} else {
+				team2.players = append(team2.players, distributedPlayer)
+			}
 		}
 	}
+}
+
+func isEven(number int) bool {
+	return number%2 == 0
 }
--- team/team_test.go
+++ team/team_test.go
@@ -9,6 +9,74 @@ import (
 )
 
 func TestDistribute(t *testing.T) {
+	goalies := []player.Player{
+		{MyClubId: 1000, Name: "Matti Meikäläinen", IsGoalie: true},
+		{MyClubId: 1001, Name: "Teppo Teikäläinen", IsGoalie: true},
+		{MyClubId: 1002, Name: "Kaija Karppi", IsGoalie: true},
+		{MyClubId: 1003, Name: "Saija Siirappi", IsGoalie: true},
+	}
+	fieldPlayers := []player.Player{
+		{MyClubId: 1004, Name: "Lauri Laavu", IsGoalie: false},
+		{MyClubId: 1005, Name: "Marja Myyry", IsGoalie: false},
+		{MyClubId: 1006, Name: "Tarja Tyyry", IsGoalie: false},
+		{MyClubId: 1007, Name: "Kalevi Kuurula", IsGoalie: false},
+	}
+	t.Run("distributing players returns even teams", func(t *testing.T) {
+		var testCases = []struct {
+			name                string
+			initialGoalies      []player.Player
+			initialFieldPlayers []player.Player
+			expectedTeam1Length int
+			expectedTeam2Length int
+		}{
+			{
+				name:                "even number of goalies and field players",
+				initialGoalies:      goalies[:4],
+				initialFieldPlayers: fieldPlayers[:4],
+				expectedTeam1Length: 4,
+				expectedTeam2Length: 4,
+			},
+			{
+				name: "zero goalies and even number of field players",
+
+				initialGoalies:      []player.Player{},
+				initialFieldPlayers: fieldPlayers[:4],
+				expectedTeam1Length: 2,
+				expectedTeam2Length: 2,
+			},
+			{
+				name: "uneven number of goalies and even number of field players",
+
+				initialGoalies:      goalies[:3],
+				initialFieldPlayers: fieldPlayers[:4],
+				expectedTeam1Length: 3,
+				expectedTeam2Length: 4,
+			},
+			{
+				name: "uneven number of goalies and uneven number of field players",
+
+				initialGoalies:      goalies[:3],
+				initialFieldPlayers: fieldPlayers[:3],
+				expectedTeam1Length: 3,
+				expectedTeam2Length: 3,
+			},
+		}
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				actualTeam1, actualTeam2, actualErr := Distribute(testCase.initialGoalies, testCase.initialFieldPlayers)
+
+				if actualErr != nil {
+					t.Errorf("unexpected error: got [%v] want [nil]", actualErr)
+				}
+				if len(actualTeam1.players) != testCase.expectedTeam1Length {
+					t.Errorf("number of players in %s don't match: got [%d] want [%d]", actualTeam1.name, len(actualTeam1.players), testCase.expectedTeam1Length)
+				}
+				if len(actualTeam2.players) != testCase.expectedTeam2Length {
+					t.Errorf("number of players in %s don't match: got [%d] want [%d]", actualTeam2.name, len(actualTeam2.players), testCase.expectedTeam2Length)
+				}
+			})
+		}
+	})
 	var tests = []struct {
 		name          string
 		goalies       []player.Player
