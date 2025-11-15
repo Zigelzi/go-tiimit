be2b6898cd5e9d3777bc467ed5dc448b129b861e
Miika Savela <miika.a.savela@gmail.com>
2025-11-15 20:34:58 +0200

    Add importing attending players from cli and web

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/Zigelzi/go-tiimit/internal/file"
 )
 
 func handleSubmitAttendanceList(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("attendace-list")
+	formFile, header, err := r.FormFile("attendace-list")
 	if err != nil {
 		log.Printf("Error parsing file from form: %v", err)
 		return
 	}
-	defer file.Close()
+	defer formFile.Close()
 	fmt.Println(header.Filename)
-
+	attendanceRows, err := file.ImportAttendancePlayerRowsFromReader(formFile)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("unable to parse the attendance rows in handler: %v", err)
+		return
+	}
+	for _, row := range attendanceRows {
+		fmt.Println(row)
+	}
 }
