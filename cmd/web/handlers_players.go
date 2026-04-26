e22394923b302a5eadaf8650ea8ce11855a4748e
Miika Savela <miika.a.savela@gmail.com>
2026-04-26 10:18:17 +0300

    Display players on the player page

--- cmd/web/handlers_players.go
+++ cmd/web/handlers_players.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/Zigelzi/go-tiimit/cmd/web/components"
@@ -8,7 +11,23 @@ import (
 )
 
 func (cfg *webConfig) handleViewPlayersPage(w http.ResponseWriter, r *http.Request) {
+
+	dbPlayers, err := cfg.queries.GetAllPlayers(r.Context())
+
+	if err != nil && errors.Is(err, sql.ErrNoRows) == false {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("failed to get players: %v", err)
+		return
+	}
+
 	players := []view.Player{}
+	for _, dbPlayer := range dbPlayers {
+		players = append(players, view.Player{
+			ID:       dbPlayer.ID,
+			Name:     dbPlayer.Name,
+			IsGoalie: dbPlayer.IsGoalie,
+		})
+	}
 	playersPage := components.PlayersPage(players)
 	playersPage.Render(r.Context(), w)
 }
